90b856bb34e9c7adaa60ae73a7e27bc596e97c46
James Ross <james@jross.me>
2025-01-01 18:42:31 +0000

    fix: cjs interop

---
+++ tests/imports.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest';
+
+import tailwindCssContentVisibilityPlugin from '../';
+
+describe('imports', () => {
+	it('esm', () => {
+		expect(tailwindCssContentVisibilityPlugin).toBeDefined();
+		expect(tailwindCssContentVisibilityPlugin.handler).toBeInstanceOf(Function);
+		expect(tailwindCssContentVisibilityPlugin.handler).toBeInstanceOf(Function);
+		expect(tailwindCssContentVisibilityPlugin.config).toHaveProperty('theme');
+		expect(tailwindCssContentVisibilityPlugin.config?.theme).toHaveProperty('contentVisibility');
+	});
+
+	it('cjs', () => {
+		// eslint-disable-next-line @typescript-eslint/no-require-imports
+		const tailwindCssContentVisibilityPluginCjs = require('../');
+		expect(tailwindCssContentVisibilityPluginCjs).toBeDefined();
+		expect(tailwindCssContentVisibilityPluginCjs.handler).toBeInstanceOf(Function);
+		expect(tailwindCssContentVisibilityPluginCjs.config).toHaveProperty('theme');
+		expect(tailwindCssContentVisibilityPluginCjs.config.theme).toHaveProperty('contentVisibility');
+	});
+});
