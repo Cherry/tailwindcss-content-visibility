22812e3a5966dcdf9951f8f62d4b50f05c5a38f3
James Ross <james@jross.me>
2025-04-05 18:27:32 +0100

    feat: support tailwind v4

--- tests/plugin.test.ts
+++ tests/plugin.test.ts
@@ -1,8 +1,11 @@
 import postcss from 'postcss';
 import tailwind from 'tailwindcss';
+import { compile } from 'tailwindcss4';
 import { describe, expect, it } from 'vitest';
 
 import tailwindCssContentVisibilityPlugin from '../src';
+
+import type { PluginsConfig } from 'tailwindcss4/plugin';
 
 const expected = `
 .content-visibility-auto {
@@ -29,7 +32,7 @@ const expected = `
 `;
 
 describe('plugin', () => {
-	it('test', async () => {
+	it('test - v3', async () => {
 		const results = await postcss([
 			tailwind({
 				content: [
@@ -59,6 +62,62 @@ describe('plugin', () => {
 		});
 
 		expect(results.css).toBe(expected.trim());
+	});
+
+	it('test - v4', async () => {
+		const results = await compile(`
+			@tailwind utilities;
+			@theme {
+				--contain-intrinsic-size-example: 1px 20px;
+			}
+			@plugin "./plugin.js";
+		`, {
+			async loadModule(id, base) {
+				return {
+					base,
+					// Types between v3 and v4 are slightly different
+					module: tailwindCssContentVisibilityPlugin as unknown as PluginsConfig,
+				};
+			},
+		});
+		const built = results.build([
+			'content-visibility-auto',
+			'content-visibility-hidden',
+			'content-visibility-visible',
+			'contain-intrinsic-size-[0px_120px]',
+			'contain-intrinsic-size-[120px_0px]',
+			'contain-intrinsic-size-[auto_120px_auto_0px]',
+			'contain-intrinsic-size-example',
+		]);
+
+		expect(built.replace(
+			/\/\*! tailwindcss v[\d.]+ \| MIT License \| https:\/\/tailwindcss\.com \*\//,
+			'/*! tailwindcss vX.Y.Z | MIT License | https://tailwindcss.com */',
+		)).toMatchInlineSnapshot(`
+			"/*! tailwindcss vX.Y.Z | MIT License | https://tailwindcss.com */
+			.contain-intrinsic-size-\\[0px_120px\\] {
+			  contain-intrinsic-size: 0px 120px;
+			}
+			.contain-intrinsic-size-\\[120px_0px\\] {
+			  contain-intrinsic-size: 120px 0px;
+			}
+			.contain-intrinsic-size-\\[auto_120px_auto_0px\\] {
+			  contain-intrinsic-size: auto 120px auto 0px;
+			}
+			.contain-intrinsic-size-example {
+			  contain-intrinsic-size: 1px 20px;
+			}
+			.content-visibility-auto {
+			  content-visibility: auto;
+			}
+			.content-visibility-hidden {
+			  content-visibility: hidden;
+			}
+			.content-visibility-visible {
+			  content-visibility: visible;
+			}
+			"
+		`);
 	});
 });
 
